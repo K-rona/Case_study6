46ed2108fb1faf17fa75eafdf03d34a920f91dc1
K-rona <126541689+K-rona@users.noreply.github.com>
2023-12-10 13:14:50 +0700

    Update filesystem.py

--- filesystem.py
+++ filesystem.py
@@ -1,47 +1,43 @@
 import os
-
-MENU = "1. Просмотр каталога\n" \
-       "2. На уровень вверх\n" \
-       "3. На уровень вниз\n" \
-       "4. Количество файлов\n" \
-       "5. Размер текущего каталога (в байтах)\n" \
-       "6. Поиск файла\n" \
-       "7. Выход из программы"
+import Text_Ru
 
 
-def AcceptCommand():
-    command = input('Выберите пункт меню: ')
+def accept_command():
+    command = input(Text_Ru.MENU_ITEM)
 
-    while command not in ['1','2','3','4','5','6','7']:
-        if command not in ['1','2','3','4','5','6','7']:
+    while command not in ['1', '2', '3', '4', '5', '6', '7']:
+        if command not in ['1', '2', '3', '4', '5', '6', '7']:
 
-            print("Неверная команда! Введите другой номер команды")
+            print(Text_Ru.WRONG_COMMAND)
 
-        command = input('Выберите пункт меню: ')
+        command = input(Text_Ru.MENU_ITEM)
 
     return int(command)
 
 
-def MoveUp():
-    dir = os.getcwd()
-    i = len(dir) - 1
-    while dir[i] != '\\':
+def move_up():
+    directory = os.getcwd()
+    i = len(directory) - 1
+    
+    while directory[i] != '\\':
         i = i - 1
-    os.chdir(dir[:i])
+    os.chdir(directory[:i])
 
-def CountFiles(path):
+
+def count_files(path):
 
     summ_file = 0
     file_list = os.listdir(path)
 
     for i in file_list:
         if os.path.isfile(path + "\\" + i):
-            summ_file +=1
+            summ_file += 1
         else:
-            summ_file += CountFiles(path + "\\" + i)
+            summ_file += count_files(path + "\\" + i)
     return summ_file
 
-def FindFiles(target, path):
+
+def find_files(target, path):
 
     list_path = []
     all_file_list = os.listdir(path)
@@ -51,97 +47,102 @@ def FindFiles(target, path):
             list_path.append(path + "\\" + i)
 
         elif os.path.isdir(path + "\\" + i):
-            if FindFiles(target,path + "\\" + i):
-                list_path.extend(FindFiles(target,path + "\\" + i))
+            if find_files(target, path + "\\" + i):
+                list_path.extend(find_files(target, path + "\\" + i))
 
     return list_path
 
-def Lookthrough(CurrentDir):
 
-    content = os.listdir(CurrentDir)
+def look_through(current_dir):
+
+    content = os.listdir(current_dir)
 
     for element in content:
 
-        if os.path.isfile(CurrentDir + "/" + element):
+        if os.path.isfile(current_dir + "/" + element):
             print(element, "," + "type = file")
 
-        if os.path.isdir(CurrentDir + "/" + element):
+        if os.path.isdir(current_dir + "/" + element):
             print(element, "," + "type = dir")
 
 
-def MoveDown(CurrentDir):
+def move_down(current_dir):
 
     test = False
 
     while not test:
 
-        subdirectory_name = input("Введите название подкаталога: ")
-        test = os.path.exists(CurrentDir + "/" + subdirectory_name)
+        subdirectory_name = input(Text_Ru.SUBDIRECTORY_NAME)
+        test = os.path.exists(current_dir + "/" + subdirectory_name)
 
         if not test:
-            print("Некорректное название каталога")
+            print(Text_Ru.ERROR_SUBDIRECTORY)
 
-    NewDir = CurrentDir + "/" + subdirectory_name
-    os.chdir(NewDir)
+    new_dir = current_dir + "/" + subdirectory_name
+    os.chdir(new_dir)
 
 
-def CountBytes(CurrentDir):
+def count_bytes(current_dir):
 
-    content = os.listdir(CurrentDir)
+    content = os.listdir(current_dir)
     summ_bytes = 0
 
     for i in content:
 
-        if os.path.isfile(CurrentDir + "/" + i):
-            summ_bytes += os.path.getsize(CurrentDir + "/" + i)
+        if os.path.isfile(current_dir + "/" + i):
+            summ_bytes += os.path.getsize(current_dir + "/" + i)
 
-        if os.path.isdir(CurrentDir + "/" + i):
-            summ_bytes += CountBytes(CurrentDir + "/" + i)
+        if os.path.isdir(current_dir + "/" + i):
+            summ_bytes += count_bytes(current_dir + "/" + i)
 
     return summ_bytes
 
-def RunCommand(command):
 
-    CurrentDir = os.getcwd()
+def run_command(command):
+
+    current_dir = os.getcwd()
 
     if command == 1:
-        Lookthrough(CurrentDir)
+        look_through(current_dir)
 
     if command == 2:
-        MoveUp(CurrentDir)
+        move_up()
 
     if command == 3:
-        MoveDown(CurrentDir)
+        move_down(current_dir)
 
     if command == 4:
-        print(CountFiles(CurrentDir))
+        print(count_files(current_dir))
 
     if command == 5:
-        print(CountBytes(CurrentDir))
+        print(count_bytes(current_dir))
 
     if command == 6:
-           target, path = input("Выберите target и path").split()
-           find_file = FindFiles(target,path)
-           if find_file:
-                  for i in find_file:
-                         print(i)
-           else:
-                  print("Файл не найден")
+        
+        target, path = input(Text_Ru.FINDING_FILES).split()
+        find_file = find_files(target, path)
+           
+        if find_file:
+            for i in find_file:
+                print(i)
+            else:
+                print(Text_Ru.FILE_ERROR)
 
 
 def main():
     while True:
 
         print(os.getcwd())
-        print(MENU)
-        command = AcceptCommand()
+        print(Text_Ru.MENU)
+        command = accept_command()
 
         if command == 7:
-            print('Работа программы завершена.')
+            print(Text_Ru.END_OF_PROGRAM)
             break
 
-        RunCommand(command)
+        run_command(command)
 
 
 if __name__ == '__main__':
     main()
+    
